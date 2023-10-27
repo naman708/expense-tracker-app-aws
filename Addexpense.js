3044e7535e505528af12c1a14852fefd2e3711b8
naman708 <83646450+naman708@users.noreply.github.com>
2023-10-27 13:44:19 +0530

    use of transaction in adding expense

--- Addexpense.js
+++ Addexpense.js
@@ -3,27 +3,46 @@ const Razorpay = require('razorpay');
 const Order = require('../models/orders')
 const SIGNIN=require('../models/signin');
 const usercontrol=require('./expensesignin');
+const sequelize = require('../util/database');
+
 
 
 exports.addexpense= async(req,res)=>{
-    try{
-        const{Price,Description,Category}=req.body;
-        const data1=await Expense.create({
-        Price:Price,
-        Description:Description,
-        Category:Category,
-        userDetailId:req.user.id,
-       });
-        
-        
-       res.status(201).json({added:data1});
+    
+    const t = await sequelize.transaction(); // Start a new transaction
 
+try {
+    const { Price, Description, Category } = req.body;
 
-    }
-    catch(err){
-        console.log(err);
-      res.status(500).send('An error occurred while saving appointments.');
-    }
+    // Create a new expense record within the transaction
+    const data1 = await Expense.create({
+        Price: Price,
+        Description: Description,
+        Category: Category,
+        userDetailId: req.user.id,
+    }, { transaction: t });
+
+    // Calculate the new total expense
+    const newTotalExpense = Number(req.user.totalExpense) + Number(Price);
+
+    // Update the user's total expense within the transaction
+    await SIGNIN.update(
+        { totalExpense: newTotalExpense },
+        { where: { id: req.user.id } },
+        { transaction: t }
+    );
+
+    console.log(`Total expense after adding: ${newTotalExpense}`);
+
+    // Commit the transaction to save changes
+    await t.commit();
+
+    res.status(201).json({ added: data1 });
+} catch (err) {
+    console.error(err);
+    await t.rollback(); // Rollback the transaction in case of an error
+    res.status(500).send('An error occurred while saving expenses.');
+}
 
 
 };
